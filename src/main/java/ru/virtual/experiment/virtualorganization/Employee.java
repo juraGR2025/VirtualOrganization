dd68f13ca6dee2fe060923ba857762749dd5f696
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2023-12-21 21:29:53 +0300

    21.12.2023

---
+++ src/main/java/ru/virtual/experiment/virtualorganization/Employee.java
@@ -0,0 +1,8 @@
+package ru.virtual.experiment.virtualorganization;
+
+public class Employee {
+    int employeeId;// Поле для id сотрудника.
+    double realEmployeeWorkloadTime;// Реальное время загруженности сотрудника.
+    private double planningHorizon;// Создается переменная для определения горизонта планирования.
+    private double[][]kompetence;// Создаем двумерный массив для хранения показателей компетенций персонала.???
+}
